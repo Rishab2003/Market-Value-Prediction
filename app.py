d3d2e1e7b5bf995c13329232f32201ba5a2324e9
Rishab <Your Email>
2023-04-20 14:50:23 +0530

    front index.html template is ready

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+model=pickle.load(open('model.pkl','rb'))
+
+
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict_placement():
+    g1=int(request.form.get('g1'))
+    g2=int(request.form.get('g2'))
+    g3=int(request.form.get('g3'))
+    g4=int(request.form.get('g4'))
+    g5=int(request.form.get('g5'))
+    g6=int(request.form.get('g6'))
+    g7=int(request.form.get('g7'))
+
+    result =model.predict(np.array([g1,g2,g3,g4,g5,g6,g7]).reshape(1,7))
+    return render_template('predict.html',result =str(result))
+    
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
